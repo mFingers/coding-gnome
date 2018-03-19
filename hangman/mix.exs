d0aeeccfc3ba2a69aa9d7cbacdac6f949416d6a7
Mike Falanga <m.falanga@gmail.com>
2018-03-19 08:59:09 -0400

    Upgrade to Elixir 1.6

--- hangman/mix.exs
+++ hangman/mix.exs
@@ -5,7 +5,7 @@ defmodule Hangman.Mixfile do
     [
       app: :hangman,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
