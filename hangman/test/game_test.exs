a76d97a3c1227ef0ea70bafb5ac569304f22b649
Mike Falanga <m.falanga@gmail.com>
2018-03-19 10:04:40 -0400

    Change API of hangman

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -19,62 +19,62 @@ defmodule Gameest do
   test "state isn't changed for a won game" do
     for state <- [ :won, :lost ] do
       game = Game.new_game |> Map.put(:game_state, state)
-      assert ^game = Game.make_move(game, "x")
+      assert { game, _ } = Game.make_move(game, "x")
     end
   end
 
   test "first occurrence of letter is not already used" do
     game = Game.new_game
-    game = Game.make_move(game, "x")
+    { game, _ } = Game.make_move(game, "x")
     assert game.game_state != :already_used
   end
 
   test "second occurrence of letter is not already used" do
     game = Game.new_game
-    game = Game.make_move(game, "x")
-    game = Game.make_move(game, "x")
+    { game, _ } = Game.make_move(game, "x")
+    { game, _ } = Game.make_move(game, "x")
     assert game.game_state == :already_used
   end
 
   test "a good guess is recognized" do
     game = Game.new_game("wibble")
-    game = Game.make_move(game, "w")
+    { game, _ } = Game.make_move(game, "w")
     assert game.game_state == :good_guess
     assert game.turns_left === 7
   end
 
   test "a guessed word is a won game" do
     game = Game.new_game("wibble")
-    game = Game.make_move(game, "w")
-    game = Game.make_move(game, "i")
-    game = Game.make_move(game, "b")
-    game = Game.make_move(game, "l")
-    game = Game.make_move(game, "e")
+    { game, _ } = Game.make_move(game, "w")
+    { game, _ } = Game.make_move(game, "i")
+    { game, _ } = Game.make_move(game, "b")
+    { game, _ } = Game.make_move(game, "l")
+    { game, _ } = Game.make_move(game, "e")
     assert game.game_state == :won
   end
 
   test "a bad guessed is recognized" do
     game = Game.new_game("wibble")
-    game = Game.make_move(game, "x")
+    { game, _ } = Game.make_move(game, "x")
     assert game.game_state == :bad_guess
     assert game.turns_left == 6
   end
 
   test "game lost" do
     game = Game.new_game("wibble")
-    game = Game.make_move(game, "a")
+    { game, _ } = Game.make_move(game, "a")
     assert game.turns_left == 6
-    game = Game.make_move(game, "h")
+    { game, _ } = Game.make_move(game, "h")
     assert game.turns_left == 5
-    game = Game.make_move(game, "c")
+    { game, _ } = Game.make_move(game, "c")
     assert game.turns_left == 4
-    game = Game.make_move(game, "d")
+    { game, _ } = Game.make_move(game, "d")
     assert game.turns_left == 3
-    game = Game.make_move(game, "j")
+    { game, _ } = Game.make_move(game, "j")
     assert game.turns_left == 2
-    game = Game.make_move(game, "f")
+    { game, _ } = Game.make_move(game, "f")
     assert game.turns_left == 1
-    game = Game.make_move(game, "g")
+    { game, _ } = Game.make_move(game, "g")
     assert game.game_state == :lost
     assert game.turns_left == 0
   end
