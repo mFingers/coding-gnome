531f9029a118ab6b79d09c6b1257be0adbe7077c
Mike Falanga <m.falanga@gmail.com>
2018-03-21 20:13:23 -0400

    Extract some helpers for a nicer UI

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,5 +1,15 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
 
+  import Gallows.Views.Helpers.GameStateHelper
+
+  def game_over?(%{ game_state: game_state }) do
+    game_state in [ :won, :lost ]
+  end
+
+  def new_game_button(conn) do
+    button("New Game", to: hangman_path(conn, :new_game))
+  end
+
   def join(characters), do: characters |> Enum.join(" ")
 end
